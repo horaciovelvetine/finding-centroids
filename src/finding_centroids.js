d898459f5d1ea532f2882bd13f17a07ec5f3a8ff
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-04-21 19:00:36 -0600

    import UI elements, add constants, move sketch to align center to center of sketch

--- src/finding_centroids.js
+++ src/finding_centroids.js
@@ -1,13 +1,18 @@
+import DisplayCartesianAxis2D from "./components/display_cartesian_axis_2d";
+import DisplayCartesianGrid from "./components/display_cartesian_grid_2d";
+import { SKETCH_HEIGHT, SKETCH_WIDTH, VERTEX_RADIUS } from "./constants";
+
 export const findingCentroids = (p5) => {
   p5.setup = () => {
-    p5.createCanvas(800, 800);
+    p5.createCanvas(SKETCH_WIDTH, SKETCH_HEIGHT);
     p5.background(200);
     p5.noLoop(); // draw() will only be called once (!!)
   }
 
   p5.draw = () => {
-    p5.background(200);
-    p5.fill(255, 0, 0);
-    p5.ellipse(400, 400, 50, 50);
+    p5.translate(SKETCH_WIDTH / 2, SKETCH_HEIGHT / 2);
+    p5.background('black');
+    DisplayCartesianAxis2D(p5);
+    DisplayCartesianGrid(p5);
   }
 }
