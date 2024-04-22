4584b6301e20a71c652e9157f76d0dd99a4ac3ca
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-04-21 19:00:47 -0600

    add spacing sizer for grid

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,6 @@
-export const SCREEN_WIDTH = 800;
-export const SCREEN_HEIGHT = 800;
+export const SKETCH_WIDTH = 1000;
+export const SKETCH_HEIGHT = 800;
+export const GRID_SPACING = 50;
 
 export const SATELLITE_VERTICES = 6;
 export const VERTEX_RADIUS = 10;
