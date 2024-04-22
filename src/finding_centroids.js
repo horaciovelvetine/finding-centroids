b99cdba2513b4b8cc06c75836240571b4e72dca8
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-04-21 21:01:16 -0600

    adds finding weighted mean calcs working

--- src/finding_centroids.js
+++ src/finding_centroids.js
@@ -1,18 +1,37 @@
+import { SKETCH_HEIGHT, SKETCH_WIDTH } from "./constants";
 import DisplayCartesianAxis2D from "./components/display_cartesian_axis_2d";
 import DisplayCartesianGrid from "./components/display_cartesian_grid_2d";
-import { SKETCH_HEIGHT, SKETCH_WIDTH, VERTEX_RADIUS } from "./constants";
+import CreateSimpleGraphset from "./functions/create_simple_graphset";
+import FindWeightedMeanVertex from "./functions/find_weighted_mean_vertex";
+
+let graphset;
 
 export const findingCentroids = (p5) => {
   p5.setup = () => {
     p5.createCanvas(SKETCH_WIDTH, SKETCH_HEIGHT);
-    p5.background(200);
+    p5.background('black');
     p5.noLoop(); // draw() will only be called once (!!)
   }
 
   p5.draw = () => {
-    p5.translate(SKETCH_WIDTH / 2, SKETCH_HEIGHT / 2);
-    p5.background('black');
+    p5.translate(SKETCH_WIDTH / 2, SKETCH_HEIGHT / 2); // Mandatory for 2D cartesian plane 
+    // Display UI Components
     DisplayCartesianAxis2D(p5);
     DisplayCartesianGrid(p5);
+    
+    // Display and Calculate the Graphset
+    graphset = CreateSimpleGraphset(p5); // Create a randomly generated graphset
+    let weightedMeanResults = FindWeightedMeanVertex(p5, graphset);
+
+    for (let vertex of graphset.vertices) {
+      vertex.draw();
+    }
+
+    for (let edge of graphset.edges) {
+      edge.drawStrengthRadius();
+      edge.drawMeanVertexConnectingLine();
+    }
+    console.log(weightedMeanResults);
+    
   }
 }
---
+++ src/functions/find_weighted_mean_vertex.js
@@ -0,0 +1,49 @@
+/**
+ * Calculates the weighted mean vertex of a graphset.
+ *
+ * @param {p5} p5 - The p5 instance.
+ * @param {Object} graphset - The graphset containing vertices and edges.
+ * @returns {Object} - An object containing the x and y coordinates of the mean vertex,
+ *                     the score, and the mean vertex object.
+ */
+import Vertex2D from "../models/vertex_2d";
+
+export default function FindWeightedMeanVertex(p5, graphset) {
+  let sumX = 0;
+  let sumY = 0;
+  let sumWeight = 0;
+  let meanVertex = new Vertex2D(p5, 0, 0, p5.color('cyan'));
+
+  for (let vertex of graphset.vertices) {
+    let vertexWeight = 0;
+    for (let edge of graphset.edges) {
+      edge.source = meanVertex;
+      if (edge.target === vertex) {
+        vertexWeight += edge.weight;
+      }
+    }
+    sumX += vertex.x * vertexWeight;
+    sumY += vertex.y * vertexWeight;
+    sumWeight += vertexWeight;
+  }
+
+  const meanAvgX = sumX / sumWeight;
+  const meanAvgY = sumY / sumWeight;
+
+  let totalDistance = 0;
+  for (let vertex of graphset.vertices) {
+    totalDistance += p5.dist(vertex.x, vertex.y, meanAvgX, meanAvgY);
+  }
+
+  const meanScore = totalDistance / graphset.vertices.length;
+  meanVertex.x = meanAvgX;
+  meanVertex.y = meanAvgY;
+  meanVertex.draw();
+
+  return {
+    x: meanAvgX,
+    y: meanAvgY,
+    score: meanScore,
+    vertex: meanVertex
+  };
+}
