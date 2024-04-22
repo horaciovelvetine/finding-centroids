5b9c95c4943994a30d1b8d90c6e57dee4cc57ff1
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-04-21 23:39:40 -0600

    adds display options to constants

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,6 @@
+export const SHOW_STRENGTH_RADIUS = false;
+export const SHOW_MEAN_VERTEX_CONNECTING_LINE = true;
+
 export const SKETCH_WIDTH = 1000;
 export const SKETCH_HEIGHT = 800;
 export const GRID_SPACING = 50;
