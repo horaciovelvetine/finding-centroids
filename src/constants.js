296b744ec8d40622f65c12a5e9e39c5e6f7f344c
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-04-21 20:59:46 -0600

    adjust names and sizes

--- src/constants.js
+++ src/constants.js
@@ -2,5 +2,5 @@ export const SKETCH_WIDTH = 1000;
 export const SKETCH_HEIGHT = 800;
 export const GRID_SPACING = 50;
 
-export const SATELLITE_VERTICES = 6;
-export const VERTEX_RADIUS = 10;
+export const TOTAL_GRAPHSET_VERTICES = 10;
+export const VERTEX_RADIUS = 15;
