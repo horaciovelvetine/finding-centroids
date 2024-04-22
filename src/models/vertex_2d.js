d5cf977cc3a7dd2fe46fe69f584367d912edde57
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-04-21 23:56:21 -0600

    vertex now draws a label to match info sidebar

--- src/models/vertex_2d.js
+++ src/models/vertex_2d.js
@@ -20,15 +20,34 @@ class Vertex2D {
   /**
    * Draws the vertex on the canvas.
    */
-  draw() {
+  draw(i) {
     const { x, y, p5, color } = this;
     if (color) {
       p5.fill(color);
     } else {
-      p5.fill(200, 170, 50, 255)
+      p5.fill(200, 170, 50, 255);
     }
     p5.ellipse(x, y, VERTEX_RADIUS, VERTEX_RADIUS);
+    p5.fill(255);
+    p5.textSize(20);
+
+    const labelOffset = VERTEX_RADIUS * 2;
+    let labelX = x + labelOffset;
+    let labelY = y;
+
+    // Adjust labelX if it would be drawn offscreen
+    if (labelX + p5.textWidth(i) > p5.width) {
+      labelX = p5.width - p5.textWidth(i);
+    }
+
+    // Adjust labelY if it would be drawn offscreen
+    if (labelY + 20 > p5.height) { // Assuming 20 is the text size
+      labelY = p5.height - 20;
+    }
+
+    p5.text(i, labelX, labelY);
   }
+
 }
 
 export default Vertex2D;
